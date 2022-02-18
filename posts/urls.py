95939840baa45bceaaf8e950ad594ab272b7c393
Antwone Adams <adams.antwone@yahoo.com>
2022-02-18 12:08:46 -0800

    Update 2

--- posts/urls.py
+++ posts/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (
 PostListView, 
 PostDetailView,
-PostCreateView
+PostCreateView,
+PostNewView,
 ) 
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name="post_list"),
     path('posts/<int:pk>/', PostDetailView.as_view(), name="post_detail"),
+    path('', PostNewView.as_view(), name="post_new"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -17,3 +17,5 @@ class PostCreateView(CreateView):
     model = Post 
     fields = ["title", "body"]
 
+
+
